0aadd6dd078eb954007b18b122979423d7e39c65
Valmor <valmormn@gmail.com>
2019-10-27 23:34:49 -0300

    dev

--- src/core/js/render/render.js
+++ src/core/js/render/render.js
@@ -1,7 +1,12 @@
 // render.js
 
-import { scanner } from "./scanner.js";
-// import { renderEngine } from "./render-engine.js";
+import {
+  scanner
+} from "./scanner.js";
+import {
+  renderEngine
+} from "./render-engine.js";
+
 
 export let render = {
   id: "render",
--- src/core/js/render/scanner.js
+++ src/core/js/render/scanner.js
@@ -1,40 +1,42 @@
 // scanner.js
 
 async function scanPage() {
-  console.log( "Escaneando a página em busca de elementos com a classe 'render'" );
+  console.log("Escaneando a página em busca de elementos com a classe 'render'");
 
   let elements2Render = [];
   elements2Render = await document.getElementsByClassName("render");
   elements2Render = Array.from(elements2Render);
 
   if (elements2Render.length > 0) {
-    elements2Render.forEach( element => {
+    elements2Render.forEach(element => {
       console.log(element);
       var event = new CustomEvent("render", {
         detail: element, // .dataset.path
         bubbles: true,
         cancelable: false
       });
-      setTimeout(()=>{element.dispatchEvent(event);}, 200);
+      setTimeout(() => {
+        element.dispatchEvent(event);
+      }, 200);
     });
   } else {
     console.log('nada para carregar por enquanto');
   }
 }
 
-// document.addEventListener("pageScan", e => {
-//   // setTimeout( scanPage(), 20 );
-//   // console.log(e);
-//   scanPage();
-// });
+document.addEventListener("pageScan", e => {
+  // setTimeout( scanPage(), 20 );
+  // console.log(e);
+  scanPage();
+});
 
 function afterLoad() {
   console.log("hey");
 
   document.addEventListener("DOMContentLoaded", function () {
     console.log('scannerx');
-    // Primeira escaneada no documento 
-    // pra carregar os primeiros componentes na pagina 
+    // Primeira escaneada no documento
+    // pra carregar os primeiros componentes na pagina
     scanPage();
   });
 }
